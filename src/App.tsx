17e909b662d583a622c7a2e2fe968f84880aa3e0
DilanBars2017 <developeryudintsev@gmail.com>
2022-06-21 13:16:58 +0300

    1-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,41 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+type getPlaceHolderObjectType= {
+    "userId": number,
+    "id": number,
+    "title":string,
+    "body": string
+}
+
 function App() {
+
+    const [posts,SetPosts]=useState<Array<getPlaceHolderObjectType>>([])
+
+    const getPlaceHolderAPI=async ()=>{
+        const result=await fetch('https://jsonplaceholder.typicode.com/posts')
+        const data=await result.json()
+        SetPosts(data)
+    }
+
+    useEffect(()=>{
+        getPlaceHolderAPI()
+    },[])
+    console.log(posts)
   return (
-
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-
+      <div className="App">
+          {
+              posts.map(element=>{
+                  return(
+                      <div>
+                          <span>{element.id}-</span>
+                          <span>{element.title}</span>
+                      </div>
+                  )
+              })
+          }
+      </div>
   );
 }
 
